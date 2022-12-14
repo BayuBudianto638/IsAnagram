4e3c4c0f8e12010d411c05068c32e7ab98dd4412
bayuhyuuga <radamanthys_ok@yahoo.com>
2022-12-14 10:11:38 +0700

    Add project files.

---
+++ IsAnagram/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Program
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string a = "abcd";
+            string b = "bcda";
+
+            var isTrue = IsAnagram(a, b);
+            Console.WriteLine($"{isTrue}");
+            Console.ReadKey();
+        }
+
+        static bool IsAnagram(this string a, string b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            if (a == b)
+                return true;
+
+            return a.OrderBy(c => c).SequenceEqual(b.OrderBy(c => c));
+        }
+    }
+}
+
